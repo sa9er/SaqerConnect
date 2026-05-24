4b39e6588c99d9153a4e125cfbff2b503f38549a
Poco x7 pro <ahmadsaqer95@outlook.com>
2026-05-24 02:47:03 +0200

    Update web app: PWA, iOS support, camera, bottom nav

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='familyconnect-v1';const urlsToCache=['/','/manifest.json','/icons/icon.svg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
